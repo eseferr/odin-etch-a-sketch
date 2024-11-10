70ea61c751fbcd51731ca20112401a20bd348186
eseferr <erginsefer@gmail.com>
2024-11-10 22:37:31 +0300

    Add Grid size changer

--- sketch.js
+++ sketch.js
@@ -1,22 +1,42 @@
-const container = document.querySelector(".container");
-for (i = 0; i < 16; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("line");
-  container.appendChild(newDiv);
-}
-
-const lines = document.querySelectorAll(".line");
-lines.forEach((element) => {
-  for (let i = 0; i < 16; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("grids");
-    element.appendChild(newDiv);
+let gridSize = 16;
+createGrid();
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  if (selectedGridSize < 100) {
+    selectedGridSize = parseInt(prompt());
+    deleteGrid();
+    createGrid(selectedGridSize);
   }
 });
 
-const grids = document.querySelectorAll(".grids");
-grids.forEach((grid) => {
-  grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = "#000";
+function createGrid(gridSize = 16) {
+  const container = document.querySelector(".grid-container");
+  for (i = 0; i < gridSize; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("line");
+    container.appendChild(newDiv);
+  }
+  const lines = document.querySelectorAll(".line");
+  lines.forEach((element) => {
+    for (let i = 0; i < gridSize; i++) {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("grids");
+      element.appendChild(newDiv);
+    }
+    draw();
   });
-});
+}
+function deleteGrid() {
+  const lines = document.querySelectorAll(".line");
+  lines.forEach((element) => {
+    element.remove();
+  });
+}
+function draw() {
+  const grids = document.querySelectorAll(".grids");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = "#000";
+    });
+  });
+}
